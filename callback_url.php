66478c94d3acafca395c84867a696eec86719adf
peetahDarius <peterdariusk@gmail.com>
2022-08-25 09:57:32 +0300

    second commit

--- callback_url.php
+++ callback_url.php
@@ -1,5 +1,5 @@
 <?php
- 		header("Content-Type: application/json");
+    header("Content-Type: application/json");
 
      $response = '{
          "ResultCode": 0, 
@@ -10,13 +10,46 @@
      $mpesaResponse = file_get_contents('php://input');
  
      // log the response
-     $logFile = "M_PESAConfirmationResponse.txt";
+     $logFile = "M_PESAConfirmationResponse.json";
+    
  
      // write to file
      $log = fopen($logFile, "a");
  
      fwrite($log, $mpesaResponse);
      fclose($log);
+
+     //Processing the Mpesa json response Data
+     $mpesaResponse = file_get_contents('M_PESAConfirmationResponse.json');
+     $callbackContent = json_decode($mpesaResponse);
+
+     $Resultcode = $callbackContent->Body->stkCallback->ResultCode;
+     $CheckoutRequestID = $callbackContent->Body->stkCallback->CheckoutRequestID;
+     $Amount = $callbackContent->Body->stkCallback->CallbackMetadata->Item[0]->Value;
+     $MpesaReceiptNumber = $callbackContent->Body->stkCallback->CallbackMetadata->Item[1]->Value;
+     $PhoneNumber = $callbackContent->Body->stkCallback->CallbackMetadata->Item[4]->Value;
+     $formatedPhone  = str_replace("254", "0" , $PhoneNumber); 
+     if ($Resultcode == 0) {
+
+         // Connect to DB
+    include('./includes/conn.inc.php');
+
+        
+  
+  
+      // Check connection
+      if ($conn->connect_error) {
+          die("<h1>Connection failed:</h1> " . $conn->connect_error);
+      } else {
+          $insert = $conn->query("INSERT INTO tinypesa(CheckoutRequestID,ResultCode,amount,MpesaReceiptNumber,PhoneNumber) VALUES ('$CheckoutRequestID','$Resultcode','$Amount','$MpesaReceiptNumber','$PhoneNumber')");
+            
+          //purge all temp data sets💀💀
+          unset($mpesaResponse[$callbackContent]);
+          echo $response;
+        
+         
+      }
+  }
+     
+
  
-     echo $response;
-?>
